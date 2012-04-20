708e575fb986ebca9bc3662952b1ec7ae3ce2697
Ross Larson <ross.e.larson@gmail.com>
2012-04-20 17:28:58 -0400

    delete Pathfinder, make Boxfinder. Starting to clean out GoogleMap2

---
+++ src/world_viewer/Boxfinder.java
@@ -0,0 +1,87 @@
+package world_viewer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.DOMException;
+
+/**
+ * @author Ross Larson and Brett Poirier
+ * @class INFO-I 400
+ */
+public class Boxfinder {
+
+	ArrayList<Box> boxList;
+	ArrayList<Photo> photos = new ArrayList<Photo>();
+	Node[][] nodes = new Node[36][36];
+	Flickr flick = new Flickr();
+
+	@SuppressWarnings("unchecked")
+	public Box[][] photoGrid = new Box[36][36];
+	public HashMap<Integer, double[]> nodeLocations = new HashMap<Integer, double[]>();
+	
+
+
+	public Boxfinder() {
+		createBoxes();
+		try {
+			getPhotos();
+		}
+		catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		catch (DOMException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createBoxes() {
+		for (int i = 0 ; i < 36 ; i++) {
+			for (int j = 0 ; j < 36 ; j++) {
+				photoGrid[i][j] = new Box(i + 36*j, new Node(getLat(j), getLong(i)));
+			}
+		}
+	}
+	
+	public double getLat(int j) {
+		return 2.5 + 5*j;
+	}
+	
+	public double getLong(int i) {
+		return 5 + 10*i;
+	}
+
+	public void getPhotos() throws XPathExpressionException, DOMException, IOException {
+		Long s = System.currentTimeMillis();
+		photos = flick.findPhotosByTags("technology");
+		Long p = System.currentTimeMillis();
+		assignPhotosToBoxes();
+		Long e = System.currentTimeMillis();
+		System.out.println("Photos: " + (p - s) + "\nAll assignments: " + (e - p) + "\nTotal: " + (e - s));
+	}
+
+	public void assignPhotosToBoxes() {
+		for (Photo p : photos) {
+			int i, j;
+			i = (int) (p.longitude % 36);
+			j = (int) (p.latitude % 36);
+			photoGrid[i][j].add(p);
+		}
+	}
+}
--- src/world_viewer/GoogleMap2.java
+++ src/world_viewer/GoogleMap2.java
@@ -36,31 +36,26 @@ import javax.swing.*;
 
 	   private JButton next = new JButton("Next Photo -->");
 	   private JButton prev = new JButton("<-- Previous Photo");
-	   private JTextField pathArea;
 	   private JLabel pLabel;
-	   private JLabel eLabel;
 	   private JLabel photoLabel;
 	   private String string = "";
-	   private Color ButtonDefault = next.getBackground();
+	   private Color ButtonDefault = Color.CYAN;
 	   
 	   private GoogleMap gps = new GoogleMap();
-	   private Pathfinder pathMaker = new Pathfinder("normal");
+	   private Boxfinder boxMaker = new Boxfinder();
 	   private ArrayList<Node> results = new ArrayList<Node>();
-	   private ArrayList<Line2D> edgeLines = new ArrayList<Line2D>();
-	   private ArrayList<Photo> pathPhotos;
+	   private ArrayList<Photo> boxPhotos;
+	   // TODO do I need this?
 	   private HashMap<String,JButton> buttons = new HashMap<String,JButton>();
 	   private ImageIcon selectedPhotoImage;
-	   private final int MID_HEIGHT = 900;
+	   private final int MID_HEIGHT = 600;
 	   private final int PHOTO_WIDTH = 500;
-	   private final int MAP_WIDTH = 900;
+	   private final int MAP_WIDTH = 600;
 	   
 		private int current;
 		
-		private Node fromNode;
-		private Node towardNode;
-		
 		public GoogleMap2() {
-
+	
 	      JPanel MapPanel = createMapPanel();
 	      JPanel photoPanel = createPhotoPanel();
 	      
@@ -78,25 +73,23 @@ import javax.swing.*;
 		   	MapPanel.setBackground(Color.WHITE);
 		   	MapPanel.setPreferredSize(new Dimension(MAP_WIDTH,MID_HEIGHT)); 
 		   	pLabel = new JLabel(selectedPhotoImage);
-		   	pLabel.setIcon(createImageIcon("http://maps.google.com/maps/api/staticmap?center=0,0&zoom=2&size=900x900&sensor=false", "map"));
+		   	pLabel.setIcon(createImageIcon("http://maps.google.com/maps/api/staticmap?center=0,0&zoom=3&size=900x900&sensor=false", "map"));
 		
 		   	MapPanel.add(pLabel, BorderLayout.CENTER);
-		   	results = new ArrayList<Node>(pathMaker.nodes.values());
+		   	results = new ArrayList<Node>(boxMaker.nodes.values());
 		
 			for (Node n : results) {
-				int x = gps.longitudeToX(n.photo.getLongitude());
-				int y = gps.latitudeToY(n.photo.getLatitude());
-				if (n.id.length() <= 5) {
-					JButton button2 = new JButton(n.id);
-					button2.setLocation(x - 23 ,y - 13);
-					button2.setSize(46,26);
-					button2.setActionCommand("selection");
-					button2.addActionListener(this);
-					
-					buttons.put(n.id, button2);
-					// System.out.println(button2.getText() + "  ,X = " + button2.getX() + "  ,Y = " + button2.getY());
-					pLabel.add(button2);
-				}
+				int x = gps.longitudeToX(n.longitude);
+				int y = gps.latitudeToY(n.latitude);
+				JButton button2 = new JButton();
+				button2.setLocation(x - 23 ,y - 13);
+				button2.setSize(46,26);
+				button2.setActionCommand("selection");
+				button2.addActionListener(this);
+				
+				buttons.put(n.toString(), button2);
+				// System.out.println(button2.getText() + "  ,X = " + button2.getX() + "  ,Y = " + button2.getY());
+				pLabel.add(button2);
 			}
 			
 		
@@ -143,7 +136,7 @@ import javax.swing.*;
 	   	String command = e.getActionCommand();
 	   	if (command.equals("selection")) {
 	   		JButton buttonClicked = (JButton) e.getSource();
-	   		Node temp = pathMaker.nodes.get(buttonClicked.getText());
+	   		Node temp = boxMaker.nodes.get(buttonClicked.getText());
 	   		// System.out.println("Button pressed. It was for the node at: " + temp.id);
 	   		if (fromNode == null) {
 	   			fromNode = temp;
@@ -159,12 +152,12 @@ import javax.swing.*;
 	   		// call dijkstra in Pathfinder to poulate pathPhotos
 	   		if (fromNode != null && towardNode != null) {
 	   			string = "";
-	   			pathMaker.dijkstra(fromNode, towardNode);
-	   			pathPhotos = pathMaker.pathPhotos;
-	   			if (pathPhotos.size() >= 1) {
-	   				photoLabel.setIcon(createImageIcon(pathPhotos.get(0).imageURL, "Edge from " + pathPhotos.get(0).from + " to " + pathPhotos.get(0).toward));
+	   			boxMaker.dijkstra(fromNode, towardNode);
+	   			boxPhotos = boxMaker.pathPhotos;
+	   			if (boxPhotos.size() >= 1) {
+	   				photoLabel.setIcon(createImageIcon(boxPhotos.get(0).imageURL, "Edge from " + boxPhotos.get(0).from + " to " + boxPhotos.get(0).toward));
 		   			current = 0;
-		   			if (pathPhotos.size() > 1) {
+		   			if (boxPhotos.size() > 1) {
 		   				next.setEnabled(true);
 		   				prev.setEnabled(false);
 		   			}
@@ -175,25 +168,25 @@ import javax.swing.*;
 		   			}
 		   			
 		   			pLabel.getGraphics().setColor(Color.GREEN);
-		   			for (int j = pathMaker.path.size() - 1; j>0; j--) {
+		   			for (int j = boxMaker.path.size() - 1; j>0; j--) {
 		   				// this will cycle through all the edges for that Node, this way you can check the ends
-		   				for (Edge ed : pathMaker.path.get(j).edges) {	
-		   					if (ed.end.equals(pathMaker.path.get(j-1))) {
+		   				for (Edge ed : boxMaker.path.get(j).edges) {	
+		   					if (ed.end.equals(boxMaker.path.get(j-1))) {
 //		   						Line2D line = new Line2D.Double(gps.longitudeToX(ed.start.photo.longitude), gps.latitudeToY(ed.start.photo.latitude),
 //		   								gps.longitudeToX(ed.end.photo.longitude), gps.latitudeToY(ed.end.photo.latitude));
 //		   						pLabel.getGraphics().drawLine((int) line.getX1(), (int) line.getY1(), (int) line.getX2(), (int) line.getY2());
-		   						string += (pathMaker.path.get(j).id + " --> ");
+		   						string += (boxMaker.path.get(j).id + " --> ");
 		   					}
 		   				}
-		   				if (j < pathMaker.path.size() - 1) {
-		   					buttons.get(pathMaker.path.get(j).id).setBackground(Color.CYAN);
+		   				if (j < boxMaker.path.size() - 1) {
+		   					buttons.get(boxMaker.path.get(j).id).setBackground(Color.CYAN);
 		   				}
-		   				else if (j == pathMaker.path.size() - 1) {
-		   					buttons.get(pathMaker.path.get(j).id).setBackground(Color.GREEN);
+		   				else if (j == boxMaker.path.size() - 1) {
+		   					buttons.get(boxMaker.path.get(j).id).setBackground(Color.GREEN);
 		   				}
 		   			}
-		   			buttons.get(pathMaker.path.get(0).id).setBackground(Color.RED);
-		   			string += (pathMaker.path.get(0).id + "\n");
+		   			buttons.get(boxMaker.path.get(0).id).setBackground(Color.RED);
+		   			string += (boxMaker.path.get(0).id + "\n");
 		   			if (pathArea != null) {
 		   				photoPanel.remove(pathArea);
 		   			}
@@ -207,15 +200,15 @@ import javax.swing.*;
 	   	}
 	   	else if (command.equals("next")) {
 	   		current++;
-	   		photoLabel.setIcon(createImageIcon(pathPhotos.get(current).imageURL, "Edge from " + pathPhotos.get(current).from + " to " + pathPhotos.get(current).toward));
-	   		if (pathPhotos.size() == (current + 1)) {
+	   		photoLabel.setIcon(createImageIcon(boxPhotos.get(current).imageURL, "Edge from " + boxPhotos.get(current).from + " to " + boxPhotos.get(current).toward));
+	   		if (boxPhotos.size() == (current + 1)) {
 	   			next.setEnabled(false);
 	   		}
 	   		prev.setEnabled(true);
 	   	}
 	   	else if (command.equals("previous")) {
 	   		current--;
-	   		photoLabel.setIcon(createImageIcon(pathPhotos.get(current).imageURL, "Edge from " + pathPhotos.get(current).from + " to " + pathPhotos.get(current).toward));
+	   		photoLabel.setIcon(createImageIcon(boxPhotos.get(current).imageURL, "Edge from " + boxPhotos.get(current).from + " to " + boxPhotos.get(current).toward));
 	   		if (current == 0) {
 	   			prev.setEnabled(false);
 	   		}
